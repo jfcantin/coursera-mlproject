c099c0ed1f66282f846ed7017016b190d6849809
Jean-Francois Cantin <jfcantin@gmail.com>
2015-04-23 17:38:02 -0600

    Tried random forest

--- submissionHelper.R
+++ submissionHelper.R
@@ -2,10 +2,10 @@
 # answers = rep("A", 20)
 # pml_write_files(answers)
 
-pml_write_files = function(x){
+pml_write_files = function(basedir="output", x){
   n = length(x)
   for(i in 1:n){
-    filename = paste0("problem_id_",i,".txt")
+    filename = paste0(basedir, "/", "problem_id_",i,".txt")
     write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
   }
 }
